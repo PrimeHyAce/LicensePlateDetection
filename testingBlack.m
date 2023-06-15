d74a40e5954f24af110afa66e4bbaa75d1b2e617
Faizeennn <muhammad1802ft.2020@student.uny.ac.id>
2023-06-15 17:05:16 +0700

    add blacks

--- testingBlack.m
+++ testingBlack.m
@@ -1,5 +1,5 @@
-img = imread('Cars419.png');
-load 'detectorWhites.mat'
+img = imread('12345.jpg');
+load 'detectorBlacks.mat'
 [bboxes, scores] = detect(detectorYOLOv2,img);
 if(~isempty(bboxes))
     img = insertObjectAnnotation(img,'rectangle',bboxes,scores,'Color','green');
